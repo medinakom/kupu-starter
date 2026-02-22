148f55da122dce522c9f61a06f6bb07350b8176a
Arief Prihasanto <aphasan57@gmail.com>
2026-02-22 21:09:01 +0700

    feat: Auto-generate applyFilter overrides for MTO fields in Facades

--- create-entity-view.sh
+++ create-entity-view.sh
@@ -353,6 +353,10 @@ import ${FACADE_BASE};
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.From;
+import jakarta.persistence.criteria.Predicate;
 
 @ApplicationScoped
 public class ${ENTITY_CAP}Facade extends ${FACADE_BASE_CLASS}<${ENTITY_CAP}> {
@@ -362,8 +366,32 @@ public class ${ENTITY_CAP}Facade extends ${FACADE_BASE_CLASS}<${ENTITY_CAP}> {
 
     @Override protected EntityManager getEntityManager() { return em; }
     public ${ENTITY_CAP}Facade() { super(${ENTITY_CAP}.class); }
-}
 JAVA
+
+# Generate applyFilter cases for MTO fields
+MTO_CASES=""
+for field in "${FIELDS_ARRAY[@]}"; do
+    IFS=':' read -r TYPE NAME MTO_FLAG <<< "$field"
+    if [ "$MTO_FLAG" = "MTO" ]; then
+        MTO_CASES+="            case \"$NAME\":"$'\n'"                return cb.equal(froms[0].get(\"$NAME\"), filterValue);"$'\n'
+    fi
+done
+
+if [ -n "$MTO_CASES" ]; then
+cat <<JAVA >> "$JAVA_DIR/dao/${ENTITY_CAP}Facade.java"
+
+    @Override
+    protected Predicate applyFilter(String filterName, Object filterValue, CriteriaQuery cq, From... froms) {
+        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
+        switch (filterName) {
+$MTO_CASES            default:
+                return super.applyFilter(filterName, filterValue, cq, froms);
+        }
+    }
+JAVA
+fi
+
+echo "}" >> "$JAVA_DIR/dao/${ENTITY_CAP}Facade.java"
 
 # 3. Generate Filter Content
 {
